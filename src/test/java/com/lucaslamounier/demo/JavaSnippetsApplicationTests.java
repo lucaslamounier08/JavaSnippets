56929628ae72d34f6aea113830e13be4306d1496
lucas.faria <lucas.faria@ambevtech.com.br>
2023-02-10 16:30:09 -0300

    first commit

---
+++ src/test/java/com/lucaslamounier/demo/JavaSnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslamounier.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
