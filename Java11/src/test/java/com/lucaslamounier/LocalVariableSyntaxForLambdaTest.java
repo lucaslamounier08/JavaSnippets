aa0f8b842430d2159e435c9654dc67b8f89a1b88
lucaslamounier08 <lucaslamounier08@gmail.com>
2023-10-18 10:21:54 -0300

    added new Java11 test - LocalVariableSyntaxForLambdaTest - shouldAcceptLocalVariableInLambda

---
+++ Java11/src/test/java/com/lucaslamounier/LocalVariableSyntaxForLambdaTest.java
@@ -0,0 +1,22 @@
+package com.lucaslamounier;
+
+import jakarta.annotation.Nonnull;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LocalVariableSyntaxForLambdaTest {
+
+    @Test
+    void shouldAcceptLocalVariableInLambda() {
+        List<String> sampleList = Arrays.asList("Java", "Kotlin");
+        String resultString = sampleList.stream()
+                .map((@Nonnull var x) -> x.toUpperCase())
+                .collect(Collectors.joining(", "));
+        assertThat(resultString).isEqualTo("JAVA, KOTLIN");
+    }
+}
