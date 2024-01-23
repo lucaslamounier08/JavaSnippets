7f1ef3232d799fc7c31c82d24f6cc35e66708b29
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-01-23 11:44:03 -0300

    certification test - added stringAndStringBuilder

--- JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson2HandlingTextTest.java
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson2HandlingTextTest.java
@@ -6,7 +6,24 @@ import org.junit.jupiter.api.Test;
 class Lesson2HandlingTextTest {
 
     @Test
-    void testTests() {
-        Assertions.assertEquals(1, 1);
+    void stringAndStringBuilder() {
+        String octalEscapes = "\377";
+        Assertions.assertEquals("ÿ", octalEscapes);
+
+        String backslashEscapes = "\b \t \n \f \r \" ' \\";
+
+        String unicodeEscapes = "\u7237";
+        Assertions.assertEquals("爷", unicodeEscapes);
+
+        // strings are immutable, mutations should create a new string
+        String s = "string";
+        s.toUpperCase(); // does nothing
+        Assertions.assertEquals("string", s);
+        s = s.toUpperCase();
+        Assertions.assertEquals("STRING", s);
+
+        // string overrides equals() and hashCode(), StringBuilder don't
     }
+
+    
 }
