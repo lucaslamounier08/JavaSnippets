87baddcff03fb74b0ca176fd9ae5baa0263b9055
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-01-23 11:26:11 -0300

    added new certification test - lesson 2 test class

---
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson2Test.java
@@ -0,0 +1,12 @@
+package com.lucaslamounier.certification;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Lesson2Test {
+
+    @Test
+    void testTests() {
+        Assertions.assertEquals(1, 1);
+    }
+}
