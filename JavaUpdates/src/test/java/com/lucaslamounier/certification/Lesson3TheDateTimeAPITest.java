a1e73903dd4229ac2c6f7db05c9e6e0bf9b6fd90
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-01-23 14:37:15 -0300

    certification test - added Lesson3 class with failing test

---
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson3TheDateTimeAPITest.java
@@ -0,0 +1,13 @@
+package com.lucaslamounier.certification;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Lesson3TheDateTimeAPITest {
+
+    @Test
+    void stringAndStringBuilder() {
+        Assertions.assertEquals("s", "ss");
+    }
+
+}
