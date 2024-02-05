953fb31185a991b2f8b7f5384d8d274da62a140e
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-02-05 20:05:40 -0300

    certification test - lesson 5

--- JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson5FlowControlWithExceptionsTest.java
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson5FlowControlWithExceptionsTest.java
@@ -3,6 +3,11 @@ package com.lucaslamounier.certification;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
@@ -10,7 +15,48 @@ class Lesson5FlowControlWithExceptionsTest {
 
     @Test
     void lesson5FlowControlWithExceptionsTest() {
-
+        // checked exceptions ->
+        // generally external, compiler expects exception handling (IOException, SQLException)
+        // unchecked exceptions ->
+        // compiler ignores (nullpointer, outofboundsindex)
     }
 
+    ///////////////////////////////////
+    // Compiler Want You To Declare The Most Generic Exception
+    ///////////////////////////////////
+
+    class SpecificException extends IOException {
+    }
+
+    void handle() throws IOException {
+        throw new SpecificException();
+    }
+
+    @Test
+    void flowControlTryWithResources() throws IOException {
+//        try (FileReader fr = new FileReader("");
+//             FileWriter fw = new FileWriter("");) {
+//        }
+    }
+
+    //////////////////////////////////////////
+    // try-with-resources only works when class implements AutoClosable (or extends Closable)
+
+    class MyResource implements AutoCloseable {
+        @Override
+        public void close() {
+            System.out.println("MyResource closing");
+        }
+    }
+
+    @Test
+    void shouldCloseResource() {
+        System.out.println("Starting...");
+        try (MyResource myResource = new MyResource()) {
+            System.out.println("started try");
+        }
+        System.out.println("Finishing method");
+
+        /// NOTE -> resources are closed in the reverse order they are in try-with-resources
+    }
 }
