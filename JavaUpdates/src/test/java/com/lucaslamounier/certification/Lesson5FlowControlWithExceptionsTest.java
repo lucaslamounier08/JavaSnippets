0bbfb7a83e0c2fc2eca474061c8c09a16efc4862
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-02-05 11:58:17 -0300

    certification test - lesson 5

---
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson5FlowControlWithExceptionsTest.java
@@ -0,0 +1,16 @@
+package com.lucaslamounier.certification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+class Lesson5FlowControlWithExceptionsTest {
+
+    @Test
+    void lesson5FlowControlWithExceptionsTest() {
+
+    }
+
+}
