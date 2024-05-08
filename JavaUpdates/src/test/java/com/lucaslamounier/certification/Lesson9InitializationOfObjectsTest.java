ae22a450aaef6f6323e9aeaaaaba40ea57f8768a
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-05-08 19:47:07 -0300

    certification test - Lesson9InitializationOfObjectsTest

---
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson9InitializationOfObjectsTest.java
@@ -0,0 +1,14 @@
+package com.lucaslamounier.certification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class Lesson9InitializationOfObjectsTest {
+
+    @Test
+    void lesson9InitializationOfObjectsTest() {
+
+
+    }
+}
