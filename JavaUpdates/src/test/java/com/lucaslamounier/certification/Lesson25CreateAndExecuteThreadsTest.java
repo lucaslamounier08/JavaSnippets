e2f5cd743e59bd58b5454770a8a480215e2be2fd
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-06-18 19:48:43 -0300

    certification test - Lesson25CreateAndExecuteThreadsTest

--- JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson25CreateAndExecuteThreadsTest.java
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson25CreateAndExecuteThreadsTest.java
@@ -3,6 +3,13 @@ package com.lucaslamounier.certification;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 @Slf4j
 class Lesson25CreateAndExecuteThreadsTest {
 
@@ -21,7 +28,42 @@ class Lesson25CreateAndExecuteThreadsTest {
 
         System.out.println("Exiting");
 
-        // 25.2 -
+        // 003. 25.2 ExecutorService and Future
+
+        // 004. 25.3 ExecutorService lifecycle - part 1
+    }
+
+    class MyTask implements Callable<String> {
+        private static int nextId = 0;
+        private int jobId = nextId++;
+
+        @Override
+        public String call() throws Exception {
+            System.out.println("Job " + jobId + " starting");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ie) {
+                System.out.println("Job " + jobId + " received shutdown request");
+                return "Job " + jobId + " early shutdown result";
+            }
+
+            if (jobId % 2 == 0) throw new Exception();
+
+            return "Job " + jobId + " normal result";
+        }
+    }
+
+    @Test
+    void testMyTask() {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        List<Future<String>> handles = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            handles.add(executorService.submit(new MyTask()));
+        }
+
+        // 005. 25.4 ExecutorService lifecycle - part 2
+
 
     }
 
