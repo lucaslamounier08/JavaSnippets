86d2aeb68066a503633704c1ff2c73d18e3bade9
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-06-18 19:50:23 -0300

    certification test - lesson 26

---
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson26ThreadSafeCodeLockingAndSynchronizationTest.java
@@ -0,0 +1,15 @@
+package com.lucaslamounier.certification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class Lesson26ThreadSafeCodeLockingAndSynchronizationTest {
+
+    @Test
+    void lesson26ThreadSafeCodeLockingAndSynchronizationTest() {
+        // 002. 26.1 Race conditions, deadlock, and livelock
+
+    }
+
+}
