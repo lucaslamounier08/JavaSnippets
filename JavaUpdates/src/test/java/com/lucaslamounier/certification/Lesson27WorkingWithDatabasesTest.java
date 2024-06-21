a82c8cb356032df0137f435a6f512d6ebb8ae100
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-06-21 15:07:37 -0300

    certification test - lesson 27

---
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson27WorkingWithDatabasesTest.java
@@ -0,0 +1,13 @@
+package com.lucaslamounier.certification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class Lesson27WorkingWithDatabasesTest {
+
+    @Test
+    void lesson27WorkingWithDatabasesTest() {
+
+    }
+}
