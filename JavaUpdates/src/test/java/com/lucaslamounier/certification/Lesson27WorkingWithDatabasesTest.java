c689e1b8b05c7f72633c48c580a07911b460e882
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-06-24 13:20:04 -0300

    certification test - lesson 27

--- JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson27WorkingWithDatabasesTest.java
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson27WorkingWithDatabasesTest.java
@@ -9,6 +9,8 @@ class Lesson27WorkingWithDatabasesTest {
     @Test
     void lesson27WorkingWithDatabasesTest() {
         // 002. 27.1 Making the connection
+
+        // 003. 27.2 Statement and ResultSet
         
     }
 }
