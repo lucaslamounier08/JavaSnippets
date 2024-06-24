c37f28538fc05b37300eea6f06369f117b12c32e
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-06-24 14:33:39 -0300

    certification test - lesson 29

--- JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson28FundamentalIOOperationsTest.java
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson28FundamentalIOOperationsTest.java
@@ -11,6 +11,6 @@ class Lesson28FundamentalIOOperationsTest {
         // 002. 28.1 Input and Output streams, Reader and Writer
 
         // 003. 28.2 BufferedReader, PrintWriter, Scanner and Charset conversions
-        
+
     }
 }
