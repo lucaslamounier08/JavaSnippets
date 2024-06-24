b131a44892fe7a72bc36f38e9529a2207f6638a3
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-06-24 13:59:07 -0300

    certification test - lesson 28

---
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson28FundamentalIOOperationsTest.java
@@ -0,0 +1,13 @@
+package com.lucaslamounier.certification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class Lesson28FundamentalIOOperationsTest {
+
+    @Test
+    void Lesson28FundamentalIOOperationsTest() {
+
+    }
+}
