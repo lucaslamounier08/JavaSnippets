03e058344895797e8d036f8b6bd1c4e1577364d8
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-06-25 14:29:02 -0300

    certification test - lesson 30

---
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson30FilesPathAndChannelTest.java
@@ -0,0 +1,13 @@
+package com.lucaslamounier.certification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class Lesson30FilesPathAndChannelTest {
+
+    @Test
+    void lesson30FilesPathAndChannelTest() {
+
+    }
+}
