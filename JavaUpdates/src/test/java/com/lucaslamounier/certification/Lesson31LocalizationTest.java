fbfbb720a97e8f2b5f8192d3453b9088931ee47d
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-06-26 13:18:55 -0300

    certification test - lesson 31

---
+++ JavaUpdates/src/test/java/com/lucaslamounier/certification/Lesson31LocalizationTest.java
@@ -0,0 +1,13 @@
+package com.lucaslamounier.certification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class Lesson31LocalizationTest {
+
+    @Test
+    void lesson31LocalizationTest() {
+
+    }
+}
